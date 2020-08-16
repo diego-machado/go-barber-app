16b6df823fa8f17f2c94bf2457b7f383425c9e1b
Diego Machado <diego.machadodarosa@gmail.com>
2020-08-16 20:58:47 -0300

    feat: add login frontend

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import imgLogo from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container />
+    <Container>
+      <Image source={imgLogo} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {console.log('teste')}}>Entrar</Button>
+    </Container>
   )
 }
 
